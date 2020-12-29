2c36468cc1cb0ca0a42b6df0cc33a73012297b4d
DhouhaNBIBA <70754078+DhouhaNBIBA@users.noreply.github.com>
2020-12-29 12:24:28 +0100

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,14 @@
+const mongoose =require('mongoose')
+const Users = mongoose.Schema({
+name : {
+    type: String,
+    required :true
+},
+userName :{
+    type : String
+},
+email :{
+    type: String
+}
+})
+module.exports=mongoose.model('Users',Users)
